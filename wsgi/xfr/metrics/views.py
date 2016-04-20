44486af63478161e1c845a67472c83513c6a2913
Eric Wright <ewright@redhat.com>
2016-04-20 08:10:44 -0400

    Backlog refinement

--- wsgi/xfr/metrics/views.py
+++ wsgi/xfr/metrics/views.py
@@ -118,7 +118,7 @@ def SprintReport(request):
     return render(request,'metrics/release.html',c)
 
 def Backlog(request):
-    story=Story.objects.filter(initialSprint=None).order_by('-businessValue','rallyNumber')
+    story=Story.objects.filter(release=None).order_by('-businessValue','rallyNumber')
     c = {'story': story, 
          'current': None,
          'header': 'Enhancement Backlog: ' + str(len(story)) + ' stories',
