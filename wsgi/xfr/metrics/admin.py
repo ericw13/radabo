fb23053e742a2221e0cc65d627415f3c090a5db6
Eric Wright <ewright@redhat.com>
2016-05-16 11:19:17 -0400

    Forgot admin screen

--- wsgi/xfr/metrics/admin.py
+++ wsgi/xfr/metrics/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Module
 
 # Register your models here.
+class ModuleAdmin(admin.ModelAdmin):
+    model = Module
+    search_fields = ['moduleName']
+
+admin.site.register(Module, ModuleAdmin)
