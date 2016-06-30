0315f3bcb539669067a6f1e13fc28a2c4203ba12
Eric Wright <ewright@redhat.com>
2016-06-30 15:42:38 -0400

    really removing it this time

--- wsgi/xfr/metrics/views.py
+++ wsgi/xfr/metrics/views.py
@@ -16,17 +16,6 @@ from datetime import timedelta
 def index(request):
     return render_to_response('metrics/index.html', {})
 
-def render_to_pdf(template_src, context_dict):
-    template = get_template(template_src)
-    context = Context(context_dict)
-    html = template.render(context)
-    result = StringIO.StringIO()
-
-    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return HttpResponse('We had some errors<pre>%s</pre>' % escape(html))
-
 def VelocityChart(request):
     kwargs = {
         'status': 'Accepted',
