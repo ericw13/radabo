7044142e4218b0613f82b4dbce8b7e1ac659cb9b
Eric Wright <ewright@redhat.com>
2016-08-11 16:26:57 -0400

    Fixed error handling on project stories for invalid epic numbers

--- app/radabo/utils.py
+++ app/radabo/utils.py
@@ -9,6 +9,10 @@ _ENH = "F1467"
 _PRJ = "F3841"
 
 def getApiKey():
+    """
+    Find the Rally WSAPI key ... I will be glad to move to OpenShift v3 and
+    get rid of all but the first option
+    """
     if 'DJANGO_SECRET_DIR' in os.environ:
         api_key = open(os.path.join(os.environ.get('DJANGO_SECRET_DIR'),'.rally')).read().strip()
     elif 'OPENSHIFT_HOMEDIR' in os.environ:
@@ -19,6 +23,9 @@ def getApiKey():
     return api_key
     
 def initRally():
+    """
+    Convenience function to get WSAPI key and instantiate a server instance
+    """
     try:
         api_key = getApiKey()
     except Exception as e:
@@ -33,28 +40,46 @@ def initRally():
     return rally
 
 def closeSession(session):
+    """
+    Function to set the end of a session used in updateStory.py
+    """
     session.endDate = timezone.now()
     session.save()
 
 def getSprint(name):
+    """
+    Function that returns a sprint object based on the name returned by Rally
+    in Iteration.Name
+    """
     try:
         return Sprint.objects.get(name=name)
     except Sprint.DoesNotExist:
         return None
 
 def getStory(name):
+    """
+    Function that returns a story object based on the rallyNumber/FormattedID
+    """
     try:
         return Story.objects.get(rallyNumber=name)
     except Story.DoesNotExist:
         return None
 
 def getRelease(name):
+    """
+    Function that returns an iteration object based on the name returned 
+    by Rally in Release.Name
+    """
     try:
         return Release.objects.get(name=name)
     except Release.DoesNotExist:
         return None
 
 def getCurrentSprint():
+    """
+    Function that returns the active sprint, based on now() being between the
+    start and end date
+    """
     now = timezone.now()
     try:
         return Sprint.objects.get(startDate__lte=now,endDate__gte=now)
@@ -62,6 +87,10 @@ def getCurrentSprint():
         return None
 
 def getCurrentRelease():
+    """
+    Function that returns the active release, based on now() being between the
+    start and end date
+    """
     now = timezone.now()
     try:
         return Release.objects.get(startDate__lte=now,endDate__gte=now)
@@ -69,12 +98,20 @@ def getCurrentRelease():
         return None
 
 def getModule(name):
+    """
+    Function that returns a module object based on the name returned by rally
+    in c_Module
+    """
     try:
         return Module.objects.get(moduleName=name)
     except:
         return None
 
 def getPriorSprint():
+    """
+    Function that returns the sprint before the current one.  This is not 
+    currently used, but is included as a partner to getPriorRelease.
+    """
     try:
         cur = getCurrentSprint()
         return Sprint.objects.filter(endDate__lt=cur.startDate).order_by('-startDate')[0]
@@ -82,6 +119,11 @@ def getPriorSprint():
         return None
 
 def getPriorRelease():
+    """
+    Function that returns the release before the current one.  This is used as
+    a default on the Enhancement Release page, as this is likely the most
+    recent Accepted release.
+    """
     try:
         cur = getCurrentRelease()
         return Release.objects.filter(endDate__lt=cur.startDate).order_by('-startDate')[0]
@@ -89,6 +131,10 @@ def getPriorRelease():
         return None
 
 def getReleaseList():
+    """
+    Function that returns a list of Release names for creating the dropdown
+    selector list.
+    """
     try:
         t=timezone.now() + timedelta(days=-182)
         return Release.objects.filter(startDate__gte=t).values_list('name',flat=True).order_by('-startDate')
@@ -96,6 +142,10 @@ def getReleaseList():
         return None
 
 def getSprintList():
+    """
+    Function that returns a list of Sprint names for creating the dropdown
+    selector list.
+    """
     try:
         t=timezone.now() + timedelta(days=-91)
         return Sprint.objects.filter(startDate__gte=t).values_list('name',flat=True).order_by('-startDate')
@@ -103,19 +153,34 @@ def getSprintList():
         return None
 
 def getModuleList():
+    """
+    Function that returns a list of Module names for creating the dropdown
+    selector list.
+    """
     try:
         return Module.objects.all().values_list('moduleName',flat=True).order_by('moduleName')
     except:
         return None
 
 def getProjectID(projectWSURL):
+    """
+    Function used to identify the project id to build a URL for the story.
+    """
     projectMatch = re.search(r'\d+$',projectWSURL)
     return projectMatch.group(0)
 
 def getStoryURL(projectID, storyID):
+    """
+    Function that builds the URL based on projectID returned by getProjectID()
+    """
     return "https://rally1.rallydev.com/#/" + str(projectID) + "d/detail/userstory/"+ str(storyID)
 
 def getSolutionSize(points, verbiage):
+    """
+    Function that returns solution size based on the story point estimate where
+    available.  Otherwise, the original S/M/L estimate is returned.  If no
+    sizing was ever done, Unknown is returned.
+    """
     if not points:
        solSize = verbiage if verbiage else 'Unknown'
     elif points <= 3:
@@ -128,6 +193,9 @@ def getSolutionSize(points, verbiage):
     return solSize
 
 def getFeatureDesc(text):
+    """
+    Function that sets storyType based on the Feature.FormattedID value.
+    """
     if text == _ENH:
         return "Enhancement"
     elif text == _PRJ:
@@ -136,6 +204,10 @@ def getFeatureDesc(text):
         return "Project Deliverable"
 
 def getLongDesc(text):
+    """
+    Function that parses the long description and ensures it fits within the
+    2000 character limit.
+    """
     x=re.sub(r'<b>Solution.*$','',text)
     if len(x) > 2000:
         return x[:1997]+"...".decode('utf-8')
@@ -143,7 +215,10 @@ def getLongDesc(text):
         return x.decode('utf-8')
 
 def createStory(story,session):
-
+    """
+    Function that will create a new Story object in the local DB if it does
+    not already exist.
+    """
     solSize = getSolutionSize(story.PlanEstimate, story.c_SolutionSize)
     tag=story.Tags[0].Name if story.Tags else None
 
@@ -180,6 +255,9 @@ def createStory(story,session):
     this.save()
 
 def updateStory(this, that, session):
+    """
+    Function that updates an existing Story object with the latest details.
+    """
     projectID = getProjectID(that.Project._ref)
     storyURL = getStoryURL(projectID, that.ObjectID)
     module = getModule(that.c_Module)
@@ -218,6 +296,9 @@ def updateStory(this, that, session):
     this.save()
 
 def fetchStoryFromRally(storyNumber):
+    """
+    Function that will get the information for a single story from Rally.
+    """
     try:
         rally = initRally()
         q="FormattedID = %s" % (storyNumber)
@@ -232,6 +313,10 @@ def fetchStoryFromRally(storyNumber):
     return response
 
 def getOrCreateStory(storyNumber):
+    """
+    Function used by the Sync Story form.  It calls fetchStoryFromRally() and
+    will then call createStory() or updateStory() as needed.
+    """
     try:
         session = Session()
         session.save()
@@ -258,6 +343,12 @@ def getOrCreateStory(storyNumber):
     return 'Y', "Success!"
 
 def storyDetail(storyNumber):
+    """
+    This function will fetch a single story from Rally and pull out the long
+    description.  It is not currently being used as this was added to the
+    database on createStory() or updateStory().  It could be used if someone
+    wants to make that function dynamic.
+    """
     try:
         response = fetchStoryFromRally(storyNumber)
     except Exception as e:
@@ -267,12 +358,17 @@ def storyDetail(storyNumber):
     return 'Y', text
 
 def getEpics():
+    """
+    Function that fetches projects from the portfolio hierarchy which are in
+    progress.
+    """
     try:
         rally = initRally()
     except Exception as e:
         return 'N', None
 
     try:
+        #E258 is the "project" that is the parent to all enhancements (F1467)
         q=['Parent.FormattedId = "I68"',
            'State.Name != "Done"',
            'FormattedID != "E258"',
@@ -283,42 +379,56 @@ def getEpics():
 
     results = []
     for item in data:
-        rec = {}
         if item.State != None:
-            rec['status'] = item.State.Name
+            status = item.State.Name
         else:
-            rec['status'] = 'Undefined'
-        rec['id'] = item.FormattedID
-        rec['name'] = item.Name
-        rec['percent'] = int(round(item.PercentDoneByStoryCount*100,0))
-        rec['count'] = item.LeafStoryCount
+            status = 'Undefined'
+        rec = {
+            'id': item.FormattedID,
+            'name': item.Name,
+            'percent': int(round(item.PercentDoneByStoryCount*100,0)),
+            'count': item.LeafStoryCount,
+            'status': status,
+        }
         results.append(rec)
 
     return 'Y', results
         
 def getProjectStories(epic):
+    """
+    Function that will get all stories belonging to the specified project/epic.
+    """
     try:
         rally = initRally()
     except Exception as e:
         return 'N', None
 
+    """
+    TODO
+    Currently, any exception encountered just gets you redirected to the
+    generic error page.  I should fix that some day.
+    """
     try:
         q=['Feature.Parent.FormattedId = "%s"' % (epic)]
         data = rally.get('User Story',query=q, fetch="FormattedID,Name,ScheduleState,PlanEstimate,Feature,Owner",order="Feature")
     except Exception as e:
         return str(e), None
 
+    if data.resultCount == 0:
+        return "Invalid project identifier: %s" % (epic), None
+
     results = []
     for item in data:
-        rec = {}
-        rec['id'] = item.FormattedID
-        rec['feature'] = item.Feature.Name
-        rec['featureid'] = item.Feature.FormattedID
-        rec['project'] = item.Feature.Parent.Name
-        rec['name'] = item.Name
-        rec['status'] = item.ScheduleState
-        rec['points'] = int(item.PlanEstimate) if item.PlanEstimate else 0
-        rec['owner'] = item.Owner.Name
+        rec = {
+            'id': item.FormattedID,
+            'feature': item.Feature.Name,
+            'featureid': item.Feature.FormattedID,
+            'project': item.Feature.Parent.Name,
+            'name': item.Name,
+            'status': item.ScheduleState,
+            'points': int(item.PlanEstimate) if item.PlanEstimate else 0,
+            'owner': item.Owner.Name,
+        }
         results.append(rec)
 
     return 'Y', results
